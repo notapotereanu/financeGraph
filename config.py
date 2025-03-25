3b77363428fc619157cfbcbe089a31acf79cdf8d
Andrian Potereanu <apotereanu@gmail.com>
2025-03-25 17:34:03 +0100

    aggiunta file inutili

--- config.py
+++ config.py
@@ -4,11 +4,20 @@ from typing import Dict, List
 # API Keys and URLs
 NEWS_API_KEY = "4f9a2b0c1efd40318cdf751d5b7444f1"
 NEWS_API_BASE_URL = "https://newsapi.org/v2/everything"
-SEC_API_KEY = "822fa29be2ffa2c5e28113bfe4f2e800edc146b40cb8983dba499d31a7348ea5"
+SEC_API_KEY = "397c17e93c15fc7ac67f7eb3643727790b500df0b1e0b3cd26bdb48bfd52f5d9"
 
 
 # Default Settings
-DEFAULT_STOCK_TICKER: List[str] = ["IOT", "AAPL"]
+DEFAULT_STOCK_TICKER: List[str] = [
+    #"TEAM", 
+    #"TPL",  
+    #"RCG",  
+    #"NEGG", 
+    #"MTDR", 
+    #"META", 
+    #"BCAT", 
+    "C", 
+]
 DEFAULT_NEWS_ARTICLES = 100
 DEFAULT_STOCK_HISTORY_DAYS = 180
 SEC_FILES_TO_ANALYSE = 50
